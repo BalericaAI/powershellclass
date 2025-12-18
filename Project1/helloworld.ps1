1dd4b088c1dc64f157453d3e9077ffcdf53a4023
BalericaAI <balericaai@gmail.com>
2025-12-18 14:01:49 -0500

    Add HelloWorld PowerShell script

---
+++ Project1/helloworld.ps1
@@ -0,0 +1,5 @@
+# HelloWorld.ps1
+# This is the simplest possible PowerShell script.
+# It writes a message to the screen so we know the script ran.
+
+Write-Output "Hello, PowerShell. I am still alive."
